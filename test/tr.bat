cdda238c9ff323688802c1a4eb8993975ce2154f
Geoff McLane <ubuntu@geoffair.info>
2017-06-03 14:49:04 +0200

    Show working tidy version

--- test/tr.bat
+++ test/tr.bat
@@ -1,8 +1,12 @@
 @setlocal
 @set TMPEXE=F:\Projects\tidy-html5\build\win64\Release\tidy.exe
 @if NOT EXIST %TMPEXE% goto NOEXE
+@call dirmin %TMPEXE%
+@%TMPEXE% -v
 
+@REM ===========================================
 %TMPEXE% %*
+@REM ===========================================
 
 @goto END
 
