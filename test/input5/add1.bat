8fa81d92470149089c382282f2f5c238f7a5af3c
Geoff McLane <ubuntu@geoffair.info>
2018-10-25 19:05:25 +0200

    add conv bat to add a test

---
+++ test/input5/add1.bat
@@ -0,0 +1,24 @@
+@setlocal
+@if NOT EXIST %1 (
+@echo Can NOT locate %1
+@exit /b 1
+)
+@if "%~2x" == "x" (
+@echo No 2nd param - the message
+@exit /b 1
+)
+
+call git add %1
+@if ERRORLEVEL 1 (
+@echo Failed to add %1!
+@exit /b 1
+)
+
+call git commit -m %2 %1
+@if ERRORLEVEL 1 (
+@echo COmmit failed...
+@exit /b 1
+)
+
+@echo Done %1 %2 ....
+
