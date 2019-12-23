290c641b840ae38197b0f5bdeedcd9532b367228
Geoff McLane <ubuntu@geoffair.info>
2019-12-23 21:06:29 +0100

    add test 848-4 js

---
+++ test/input5/in_848-4.js
@@ -0,0 +1,13 @@
+
+customElements.define('element-details',
+  class extends HTMLElement {
+    constructor() {
+      super();
+      const template = document
+        .getElementById('element-details-template')
+        .content;
+      const shadowRoot = this.attachShadow({mode: 'open'})
+        .appendChild(template.cloneNode(true));
+  }
+});
+
