1bd58af5faac5d6c4d3259cd6f4a1e79be2b461f
Geoff R. McLane <ubuntu@geoffair.info>
2021-04-04 16:56:59 +0200

    Some unix bld fixes

--- build/build-me.sh
+++ build/build-me.sh
@@ -8,15 +8,57 @@ RUNBIN=0
 TMPDAT=`date +%Y/%m/%d`
 TMPTIM=`date +%H:%M:%S`
 
-TMPTOPS=
-for arg in $*; do
-    TMPOPTS="$TMPOPTS $arg"
+wait_for_input()
+{
+    if [ "$#" -gt "0" ] ; then
+        echo "$1"
+    fi
+    echo -n "Enter y to continue : "
+    read char
+    if [ "$char" = "y" -o "$char" = "Y" ]
+    then
+        echo "Got $char ... continuing ..."
+    else
+        if [ "$char" = "" ] ; then
+            echo "Aborting ... no input!"
+        else
+            echo "Aborting ... got $char!"
+        fi
+        exit 1
+    fi
+    # exit 0
+}
+
+
+BLDDBG=0
+TMPSRC=".."
+TMPOPTS=""
+##############################################
+### ***** NOTE THIS INSTALL LOCATION ***** ###
+### Change to suit your taste, environment ###
+TMPINST="/usr/local"
+TMPOPTS="$TMPOPTS -DCMAKE_INSTALL_PREFIX=$TMPINST"
+##############################################
+
+### Accept user argument
+for arg in $@; do
+      case $arg in
+         VERBOSE) TMPOPTS="$TMPOPTS -DCMAKE_VERBOSE_MAKEFILE=ON" ;;
+         DEBUG) BLDDBG=1 ;;
+         SHARED) TMPOPTS="$TMPOPTS -DBUILD_SHARED_LIB:BOOL=TRUE" ;;
+         *) TMPOPTS="$TMPOPTS $arg" ;;
+      esac
 done
 
-if [ -z "$TMPOPTS" ]; then
-    # if no command line option, at least build RELEASE
-    TMPOPTS="-DCMAKE_BUILD_TYPE=Release"
+if [ "$BLDDBG" = "1" ]; then
+    TMPOPTS="$TMPOPTS -DCMAKE_BUILD_TYPE=Debug -DENABLE_DEBUG_SYMBOLS:BOOL=TRUE"
+else
+    TMPOPTS="$TMPOPTS -DCMAKE_BUILD_TYPE=Release"
 fi
+
+echo "$BN: Will do: 'cmake $TMPSRC $TMPOPTS' to $BLDLOG"
+wait_for_input
+
 
 if [ -f "$TMPLOG" ]; then
     rm -f $TMPLOG
@@ -25,9 +67,9 @@ fi
 echo "$BN: Build of tidy-test on $TMPDAT $TMPTIM - output to $TMPLOG"
 echo "$BN: Build of tidy-test on $TMPDAT $TMPTIM" > $TMPLOG
 
-echo "$BN: Doing 'cmake .. $TMPOPTS'"
-echo "$BN: Doing 'cmake .. $TMPOPTS'" >> $TMPLOG
-cmake ..  $TMPOPTS >> $TMPLOG 2>&1
+echo "$BN: Doing 'cmake $TMPSRC $TMPOPTS'"
+echo "$BN: Doing 'cmake $TMPSRC $TMPOPTS'" >> $TMPLOG
+cmake $TMPSRC  $TMPOPTS >> $TMPLOG 2>&1
 if [ ! "$?" = "0" ]; then
     echo "$BN: cmake config/gen error - see $TMPLOG"
     exit 1
